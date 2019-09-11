31bec222a997c0382b44a46fbed0ac0ec47b59dd
ladyusa <usa.sa@ku.th>
2019-09-11 19:15:15 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring Boot with JPA Example
+
+ตัวอย่างการสร้าง web service เล็ก ๆ (microservice) ด้วย Spring Boot
+โดยใช้่ JPA ในการจัดการข้อมูลในฐานข้อมูล
---
+++ src/main/java/th/ku/bookstoreservicejpa/BookstoreservicejpaApplication.java
@@ -0,0 +1,13 @@
+package th.ku.bookstoreservicejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreservicejpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreservicejpaApplication.class, args);
+	}
+
+}
