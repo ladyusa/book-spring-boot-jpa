78a2d3a75176a94857540b9809673708b8fbbb48
ladyusa <usa.sa@ku.th>
2019-10-28 22:06:42 +0700

    make db autoincrement id

--- src/main/java/th/ku/bookstoreservicejpa/Book.java
+++ src/main/java/th/ku/bookstoreservicejpa/Book.java
@@ -1,12 +1,15 @@
 package th.ku.bookstoreservicejpa;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Book {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
